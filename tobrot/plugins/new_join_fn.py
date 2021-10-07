30b84aad4fdc353152303930850f6c6e90562952
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-10-07 19:32:07 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,6 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        """join this group for help-- @GbotStoreSupport\n\n And also don't forget to star/fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""",
+        """Join Channel: @lkhitech""",
         disable_web_page_preview=True,
     )
